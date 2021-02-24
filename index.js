0c94912cedf39699ab666ff468fe94a9910cef72
Jorge Aquino <jaquino6286@gmail.com>
2021-02-23 19:38:39 -0500

    update styling

--- index.js
+++ index.js
@@ -58,6 +58,19 @@ function getCollection() {
     return images;
 }
 
+(function() {
+    var h, a, f;
+    a = document.getElementsByTagName('link');
+    for (h = 0; h < a.length; h++) {
+      f = a[h];
+      if (f.rel.toLowerCase().match(/stylesheet/) && f.href) {
+        var g = f.href.replace(/(&|\?)rnd=\d+/, '');
+        f.href = g + (g.match(/\?/) ? '&' : '?');
+        f.href += 'rnd=' + (new Date().valueOf());
+      }
+    } // for
+  })()
+
 /*
 figure out how to generate all divs with attributes from directory!
 
